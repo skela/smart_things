a0f7b257abb9852b0df85015dd4cc1adfe36ac9b
Alek Slater <skela@davincium.com>
2019-01-12 21:42:17 +0100

    Aqara button seems to work now

--- devicetypes/skela/aqara-button.src/aqara-button.groovy
+++ devicetypes/skela/aqara-button.src/aqara-button.groovy
@@ -1,7 +1,5 @@
 /**
- *  Aqara Button
- *
- *  Copyright 2019 Alek Slater
+ *  Copyright 2015 SmartThings
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -13,14 +11,28 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  */
-metadata {
-	definition (name: "Aqara Button", namespace: "skela", author: "Alek Slater") {
-		capability "Actuator"
-        capability "Momentary"
-        capability "Button"
-	}
 
-	// UI tile definitions
+metadata {
+    definition (name: "Aqara Button", namespace: "skela", author: "Alek Slater") {
+        capability "Actuator"                
+        capability "Momentary"
+        //capability "Switch Level"
+        capability "Button"
+        //capability "Light"
+	
+		// Aqara Button - original revision - model WXKG11LM
+        fingerprint endpointId: "01", profileId: "0104", deviceId: "5F01", inClusters: "0000,FFFF,0006", outClusters: "0000,0004,FFFF", manufacturer: "LUMI", model: "lumi.sensor_switch.aq2", deviceJoinName: "Aqara Button WXKG11LM"
+        // Aqara Button - new revision - model WXKG12LM
+        fingerprint endpointId: "01", profileId: "0104", deviceId: "5F01", inClusters: "0000,0001,0006,0012", outClusters: "0000", manufacturer: "LUMI", model: "lumi.sensor_switch.aq3", deviceJoinName: "Aqara Button WXKG12LM"
+        fingerprint endpointId: "01", profileId: "0104", deviceId: "5F01", inClusters: "0000,0001,0006,0012", outClusters: "0000", manufacturer: "LUMI", model: "lumi.sensor_swit", deviceJoinName: "Aqara Button WXKG12LM"
+        // Aqara Smart Light Switch - single button - model WXKG03LM
+        fingerprint endpointId: "01", profileId: "0104", deviceId: "5F01", inClusters: "0000,0003,0019,0012,FFFF", outClusters: "0000,0003,0004,0005,0019,0012,FFFF", manufacturer: "LUMI", model: "lumi.sensor_86sw1lu", deviceJoinName: "Aqara Switch WXKG03LM"
+        fingerprint endpointId: "01", profileId: "0104", deviceId: "5F01", inClusters: "0000,0003,0019,0012,FFFF", outClusters: "0000,0003,0004,0005,0019,0012,FFFF", manufacturer: "LUMI", model: "lumi.sensor_86sw1", deviceJoinName: "Aqara Switch WXKG03LM"
+        // Aqara Smart Light Switch - dual button - model WXKG02LM
+        fingerprint endpointId: "01", profileId: "0104", deviceId: "5F01", inClusters: "0000,0003,0019,0012,FFFF", outClusters: "0000,0003,0004,0005,0019,0012,FFFF", manufacturer: "LUMI", model: "lumi.sensor_86sw2Un", deviceJoinName: "Aqara Switch WXKG02LM"
+    }
+
+    // UI tile definitions
 	tiles(scale: 2){
 		multiAttributeTile(name:"switch", type: "generic", width: 6, height: 4, canChangeIcon: true){
 			tileAttribute("device.switch", key: "PRIMARY_CONTROL") {
@@ -33,38 +45,36 @@ metadata {
 }
 }
 
-def push() 
-{
-	def date = new Date()
-	def timestamp = date.getTime()/1000
-	log.debug "pushing ${timestamp}"
+def push() {
+	//def date = new Date()
+	//def timestamp = date.getTime()/1000
+	//log.debug "pushing ${timestamp}"
 	//sendEvent(name: "switch", value: "on", isStateChange: true, displayed: false)
 	//sendEvent(name: "switch", value: "off", isStateChange: true, displayed: false)
-	//sendEvent(name: "momentary", value: "pushed", isStateChange: true)
-    sendEvent(name:"level",value:timestamp)
+	sendEvent(name: "button", value: "pushed", isStateChange: true)
+    //sendEvent(name:"level",value:timestamp)
 }
 
-def on() 
-{
+def on() {
 	log.debug "im on"
 	push()
 }
 
-def off() 
-{
+def off() {
 	log.debug "im off"
 	push()
 }
 
-// parse events into attributes
-def parse(String description) {
-	log.debug "Parsing '${description}'"
-	// TODO: handle 'level' attribute
-	//push()
-}
-
-// handle commands
-def setLevel() {
-	log.debug "Executing 'setLevel'"
-	// TODO: handle 'setLevel' command
+// Parse incoming device messages to generate events
+def parse(String description) 
+{
+    log.debug "description is $description"
+    if (description=="on/off: 1")
+    {
+    	push()
+        //def ev = createEvent(name: "button", value: "pushed", data: [])
+        //sendEvent(ev)
+        //return ev
+        
+    }    
 }
